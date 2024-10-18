621d53dfb86a6c76c513fb670f454e61160fc5d9
Bnaveen <naveenbala2701@gmail.com>
2024-10-18 06:04:34 +0530

    dd

--- back.php
+++ back.php
@@ -1,10 +1,10 @@
 <?php
-if(!isset($_POST['login']))
+if(!isset($_POST['submit']))
 {
     $usr=$_POST['usr'];
     $ped=$_POST['pwd'];
 $con=mysqli_connect("localhost:3306","root","","log");
-$sql="SELECT * from detail WHERE usr='$usr' AND pwd='$pwd'";
+$sql="SELECT * from details WHERE usr='$usr' AND pwd='$pwd'";
 $result=mysqli_query($con,$sql);
 $resultcheck=mysqli_num_rows($result);
 if($resultcheck>0)

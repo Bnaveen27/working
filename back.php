94526954b7fbd2ee5766b668a46edb6c04c6162f
Bnaveen <naveenbala2701@gmail.com>
2024-10-18 05:58:07 +0530

    new

---
+++ back.php
@@ -0,0 +1,19 @@
+<?php
+if(!isset($_POST['login']))
+{
+    $usr=$_POST['usr'];
+    $ped=$_POST['pwd'];
+$con=mysqli_connect("localhost:3306","root","","log");
+$sql="SELECT * from detail WHERE usr='$usr' AND pwd='$pwd'";
+$result=mysqli_query($con,$sql);
+$resultcheck=mysqli_num_rows($result);
+if($resultcheck>0)
+{
+    echo "login sucessful";
+}    
+else
+{
+    echo "username or password incorrect";
+}
+}
+?>
